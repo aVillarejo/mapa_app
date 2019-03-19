e0fefc812146f9d38c0c94186fd70f16001c8666
“a_villarejo19” <a.villarejo19@gmail.com>
2019-03-18 18:36:41 -0600

    Updates

---
+++ precache-manifest.032937b87857067ec927b568a1ad8b24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbd3a31d3d106c0306753f9eb9510135",
+    "url": "/mapa_app/static/media/background1.dbd3a31d.jpg"
+  },
+  {
+    "revision": "7aa73480d7730edeee9f",
+    "url": "/mapa_app/static/js/runtime~main.a53945b1.js"
+  },
+  {
+    "revision": "8787251255e672853dd4",
+    "url": "/mapa_app/static/js/main.23050de6.chunk.js"
+  },
+  {
+    "revision": "26299e67868088c82353",
+    "url": "/mapa_app/static/js/2.73f5b491.chunk.js"
+  },
+  {
+    "revision": "8787251255e672853dd4",
+    "url": "/mapa_app/static/css/main.4832dc00.chunk.css"
+  },
+  {
+    "revision": "26299e67868088c82353",
+    "url": "/mapa_app/static/css/2.08cf2240.chunk.css"
+  },
+  {
+    "revision": "2b49e05348d344eec4fa7d109b08a20f",
+    "url": "/mapa_app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/mapa_app/precache-manifest.032937b87857067ec927b568a1ad8b24.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/mapa_app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
