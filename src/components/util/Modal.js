585a23bd98be89d3aa215409993ada8ba9e6d0e1
“a_villarejo19” <a.villarejo19@gmail.com>
2019-03-18 18:28:35 -0600

    init

---
+++ src/components/util/Modal.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
+
+export default class ModalComponent extends Component {
+  state = {
+    isOpen: this.props.isOpen ? this.props.isOpen : false,
+    title: this.props.title ? this.props.title : "",
+    body: this.props.body ? this.props.body : ""
+  };
+
+  toggle = () => {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen
+    }));
+  };
+
+  render() {
+    return (
+      <Modal
+        isOpen={this.state.isOpen}
+        toggle={this.toggle}
+        className={this.props.className}
+      >
+        <ModalHeader toggle={this.toggle}>{this.state.title}</ModalHeader>
+        <ModalBody>{this.state.body}</ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={this.toggle}>
+            Aceptar
+          </Button>{" "}
+          {/* <Button color="secondary" onClick={this.toggle}>
+            Cancel
+          </Button> */}
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
---
+++ src/views/pages/Register.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+import {
+  Button,
+  Container,
+  Alert,
+  InputGroup,
+  InputGroupAddon,
+  Input,
+  Card,
+  Row,
+  Col,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Form,
+  CardHeader
+} from "reactstrap";
+import imgUrl from "../../assets/background1.jpg";
+
+import Modal from "./../../components/util/Modal";
+
+class Register extends Component {
+  state = {
+    alertVisible: false
+  };
+
+  handleAlert = () => {
+    this.setState({
+      alertVisible: !this.state.alertVisible
+    });
+  };
+  render() {
+    return (
+      <Container style={styles.root}>
+        <Modal
+          isOpen={false}
+          title="Registro Exitoso"
+          body="Su cuenta ha sido registrada, gracias por su tiempo"
+        />
+        <h1>Pagina de Registo</h1>
+        <Button color="primary">Button</Button>
+        <Button color="info" onClick={this.handleAlert}>
+          Button
+        </Button>
+        <Alert
+          isOpen={this.state.alertVisible}
+          toggle={this.handleAlert}
+          color="primary"
+        >
+          Esto es un Alert
+        </Alert>
+        <br />
+        <Row>
+          <Col sm={{ size: 6, order: 2, offset: 6 }}>
+            <Card body>
+              <CardHeader>Header</CardHeader>
+              <CardTitle>Special Title Treatment</CardTitle>
+              <CardText>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </CardText>
+              <Form>
+                <InputGroup className="my-2">
+                  <InputGroupAddon addonType="prepend">N</InputGroupAddon>
+                  <Input placeholder="Nombre" />
+                </InputGroup>
+                <InputGroup className="my-3">
+                  <InputGroupAddon addonType="prepend">A</InputGroupAddon>
+                  <Input placeholder="Apellidos" />
+                </InputGroup>
+                <InputGroup className="my-3">
+                  <InputGroupAddon addonType="prepend">T</InputGroupAddon>
+                  <Input placeholder="Telefono" />
+                </InputGroup>
+                <InputGroup className="my-3">
+                  <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+                  <Input placeholder="correo" />
+                </InputGroup>
+                <InputGroup className="my-3">
+                  <InputGroupAddon addonType="prepend">*</InputGroupAddon>
+                  <Input placeholder="Password" />
+                </InputGroup>
+                <InputGroup className="my-3">
+                  <InputGroupAddon addonType="prepend">*</InputGroupAddon>
+                  <Input placeholder="Password" />
+                </InputGroup>
+                <Button block color="success">
+                  {" "}
+                  Registrarme!
+                </Button>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+let styles = {
+  root: {
+    backgroundImage: "url(" + imgUrl + ")",
+    backgroundSize: "cover",
+    overflow: "hidden"
+  }
+};
+export default Register;
